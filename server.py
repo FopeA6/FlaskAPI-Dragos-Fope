24c1552bc33ca7af61858919c1fbd62f70d4dc68
Fope <fopeayo@hotmail.com>
2021-01-19 17:39:36 +0000

    server

--- server.py
+++ server.py
@@ -2,5 +2,13 @@ from flask import Flask
 
 server = Flask(__name__)
 
+server.config["DEBUG"] = True
+
+
+@server.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
 if __name__ == "__main__":
     server.run(debug=True)
